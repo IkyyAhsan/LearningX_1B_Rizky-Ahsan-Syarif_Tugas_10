9760ef49f6e5ed31e0b6078148029d39f3d0cee3
IkyyAhsan <IkyyAhsan>
2024-05-02 17:05:50 +0700

    navbar don4t

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -7,6 +7,7 @@ abstract class Routes {
   static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
   static const LOGIN_SCREEN = _Paths.LOGIN_SCREEN;
   static const SIGNIN_SCREEN = _Paths.SIGNIN_SCREEN;
+  static const NAVIGATION_BAR = _Paths.NAVIGATION_BAR;
 }
 
 abstract class _Paths {
@@ -15,4 +16,5 @@ abstract class _Paths {
   static const SPLASH_SCREEN = '/splash-screen';
   static const LOGIN_SCREEN = '/login-screen';
   static const SIGNIN_SCREEN = '/signin-screen';
+  static const NAVIGATION_BAR = '/navigation-bar';
 }
